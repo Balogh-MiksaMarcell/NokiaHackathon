48865ffe5fcec6de92e566b730efb648b3f85fdc
Marci <Marci@DESKTOP-J8H9JJS>
2026-04-18 17:39:01 +0200

    min_num_of_drops megirasa

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,5 +1,19 @@
 from pathlib import Path
 
+def min_num_of_drops(devices, height):
+    if devices == 1:
+        return height
+    
+    reach = [0] * (devices + 1)
+    attempts = 0
+
+    while reach[devices] < height:
+        attempts += 1
+
+        for i in range(devices, 0, -1):
+            reach[i] = reach[i] + reach[i-1] + 1
+    
+    return attempts
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
