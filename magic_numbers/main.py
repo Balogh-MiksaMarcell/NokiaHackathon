b437e713c8d30eef762f647b1ecf4f2f9b8c9517
Marci <Marci@DESKTOP-J8H9JJS>
2026-04-18 15:51:49 +0200

    next_magic_number def megirasa

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,5 +1,28 @@
 from pathlib import Path
+import math
 
+def next_magic_number(number):
+    numberAsString = str(number)
+    lenght = len(numberAsString)
+
+    if(numberAsString == str(9) * lenght):
+        nextNum = number + 2
+        return nextNum
+    cuttingPoint = math.ceil(lenght / 2)
+    part1Str = numberAsString[:cuttingPoint]
+    
+    def mirrored(part):
+        if lenght % 2 == 0:
+            return part[::-1]
+        else:
+            return part[:-1][::-1]
+    
+    nextNum = part1Str + mirrored(part1Str)
+    if int(nextNum) <= number:
+        newPart1 = str(int(part1Str) + 1)
+        nextNum = newPart1 + mirrored(newPart1)
+
+    return nextNum
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
